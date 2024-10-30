37474a0f80c7e22b099f8aca82eb5a7b36d35457
강민석 <kms860926@gmail.com>
2024-10-30 17:31:51 +0900

    43228. 입국심사 풀이

--- 코딩 기초 트레이닝/181944. 홀짝 구분하기/Solution 2.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Solution {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        System.out.println(String.format("%d is %s", n , (n % 2 == 0) ? "even" : "odd"));
-    }
-}
